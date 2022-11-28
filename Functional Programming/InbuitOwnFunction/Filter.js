08a699ed3ee28929a4d62a5df62698f6a4c8c7c6
Mamata <mamata.barman@borderfree.io>
2022-11-29 03:10:40 +0530

    own filter

--- Functional Programming/InbuitOwnFunction/Filter.js
+++ Functional Programming/InbuitOwnFunction/Filter.js
@@ -15,3 +15,16 @@ let array = [1, 2, 3];
 
 let result = doSomething(array, myFilter);
 console.log("result", result);
+
+Array.prototype.myFilter = function doSomething1(callback, thisArg) {
+  let newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      newArray.push(this[i]);
+    }
+  }
+  return newArray;
+};
+
+const res3 = array.myFilter((num) => num > 1);
+console.log("res3", res3);
